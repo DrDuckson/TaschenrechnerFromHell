d5e15d6b9ad4f62aabb27c359829b492afe63b1e
DrDuckson <gorealex95@web.de>
2024-05-09 17:03:37 +0200

    Zahlen 3-9 ergänzt, "Mal" und "Geteilt" Operatoren hinzugefügt

--- Taschenrechner.pas
+++ Taschenrechner.pas
@@ -7,40 +7,44 @@ uses
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls;
 
 type
-  TForm1 = class(TForm)
-    Button1: TButton;
-    Button2: TButton;
-    Label1: TLabel;
-    Label2: TLabel;
-    Button3: TButton;
-    Button4: TButton;
-    Button5: TButton;
-    Button6: TButton;
-    Button7: TButton;
-    Button8: TButton;
-    Button9: TButton;
-    Button10: TButton;
-    Label3: TLabel;
-    Label4: TLabel;
-    Button11: TButton;
-    Button12: TButton;
-    Button13: TButton;
-    Label5: TLabel;
-    Button14: TButton;
-    procedure Button1Click(Sender: TObject);
-    procedure Button2Click(Sender: TObject);
-    procedure Button3Click(Sender: TObject);
-    procedure Button4Click(Sender: TObject);
-    procedure Button5Click(Sender: TObject);
-    procedure Button6Click(Sender: TObject);
-    procedure Button7Click(Sender: TObject);
-    procedure Button8Click(Sender: TObject);
-    procedure Button9Click(Sender: TObject);
-    procedure Button10Click(Sender: TObject);
-    procedure Button11Click(Sender: TObject);
-    procedure Button12Click(Sender: TObject);
-    procedure Button13Click(Sender: TObject);
-    procedure Button14Click(Sender: TObject);
+  TRechner = class(TForm)
+    btnZahl1: TButton;
+    btnZahl2: TButton;
+    lblEingabe1: TLabel;
+    lblOperator: TLabel;
+    btnZahl3: TButton;
+    btnZahl4: TButton;
+    btnZahl5: TButton;
+    btnZahl6: TButton;
+    btnZahl7: TButton;
+    btnZahl8: TButton;
+    btnZahl9: TButton;
+    btnZahl0: TButton;
+    lblEingabe2: TLabel;
+    lblErgebnis: TLabel;
+    btnAddition: TButton;
+    btnSubtraktion: TButton;
+    btnGleichzeichen: TButton;
+    lblGleichzeichen: TLabel;
+    btnClean: TButton;
+    btnMultiplikation: TButton;
+    btnTeilen: TButton;
+    procedure btnZahl1Click(Sender: TObject);
+    procedure btnZahl2Click(Sender: TObject);
+    procedure btnZahl3Click(Sender: TObject);
+    procedure btnZahl4Click(Sender: TObject);
+    procedure btnZahl5Click(Sender: TObject);
+    procedure btnZahl6Click(Sender: TObject);
+    procedure btnZahl7Click(Sender: TObject);
+    procedure btnZahl8Click(Sender: TObject);
+    procedure btnZahl9Click(Sender: TObject);
+    procedure btnZahl0Click(Sender: TObject);
+    procedure btnAdditionClick(Sender: TObject);
+    procedure btnSubtraktionClick(Sender: TObject);
+    procedure btnGleichzeichenClick(Sender: TObject);
+    procedure btnCleanClick(Sender: TObject);
+    procedure btnMultiplikationClick(Sender: TObject);
+    procedure btnTeilenClick(Sender: TObject);
   private
     { Private-Deklarationen }
   public
@@ -48,117 +52,231 @@ type
   end;
 
 var
-  Form1: TForm1;
-  iZahl1, iZahl2, iZahl3, iZahl4, iZahl5, iZahl6, iZahl7, iZahl8, iZahl9, iZahl10 : Integer;
-  iEingabe1, iEingabe2, iErgebnis : Integer;
-  bOperator : Boolean;
+  Rechner: TRechner;
+  iZahl1, iZahl2, iZahl3, iZahl4, iZahl5, iZahl6, iZahl7, iZahl8, iZahl9, iZahl0: Integer;
+  iEingabe1, iEingabe2, iErgebnis: Integer;
+  dEingabe1, dEingabe2, dErgebnis: double;
+  bOperator: Boolean;
+  sAusgabe: String;
 
 implementation
 {$R *.dfm}
 
-procedure TForm1.Button11Click(Sender: TObject);
+procedure TRechner.btnAdditionClick(Sender: TObject);
 begin
-  Label2.Caption := '+';
+  lblOperator.Caption := '+';
   bOperator := True;
 end;
 
-procedure TForm1.Button12Click(Sender: TObject);
+procedure TRechner.btnSubtraktionClick(Sender: TObject);
 begin
-  Label2.Caption := '-';
+  lblOperator.Caption := '-';
   bOperator := True;
 end;
 
-procedure TForm1.Button13Click(Sender: TObject);
+procedure TRechner.btnTeilenClick(Sender: TObject);
+begin
+  lblOperator.Caption := '/';
+  bOperator := True;
+end;
+
+procedure TRechner.btnGleichzeichenClick(Sender: TObject);
 begin
   begin
-  if Label2.Caption = '+' then
-   iErgebnis := iEingabe1 + iEingabe2
-   else if Label2.Caption = '-' then
-    iErgebnis := iEingabe1 - iEingabe2;
+  if lblOperator.Caption = '+' then
+     begin
+     iErgebnis := iEingabe1 + iEingabe2;
+     sAusgabe := iErgebnis.ToString;
+     end
+   else if lblOperator.Caption = '-' then
+     begin
+     iErgebnis := iEingabe1 - iEingabe2;
+     sAusgabe := iErgebnis.ToString;
+     end
+   else if lblOperator.Caption = 'x' then
+     begin
+     iErgebnis := iEingabe1 * iEingabe2;
+     sAusgabe := iErgebnis.ToString;
+     end
+   else if lblOperator.Caption = '/' then
+     begin
+     dEingabe1 := iEingabe1;
+     dEingabe2 := iEingabe2;
+     dErgebnis := iErgebnis;
+     dErgebnis := dEingabe1 / dEingabe2;
+     sAusgabe := dErgebnis.ToString;
+     end;
   end;
-  Label4.Caption := IntToStr(iErgebnis);
+  lblErgebnis.Caption := sAusgabe;
   bOperator := False;
 end;
 
-procedure TForm1.Button14Click(Sender: TObject);
+procedure TRechner.btnMultiplikationClick(Sender: TObject);
 begin
-    iErgebnis := 0;
-    iEingabe1 := 0;
-    iEingabe2 := 0;
-    Label1.Caption := '';
-    Label3.Caption := '';
-    Label4.Caption := '';
+  lblOperator.Caption := 'x';
+  bOperator := True;
 end;
 
-procedure TForm1.Button1Click(Sender: TObject);
+procedure TRechner.btnCleanClick(Sender: TObject);
+begin
+  iErgebnis := 0;
+  iEingabe1 := 0;
+  iEingabe2 := 0;
+  lblEingabe1.Caption := '';
+  lblEingabe2.Caption := '';
+  lblErgebnis.Caption := '';
+  lblOperator.Caption := '';
+  bOperator := False;
+end;
+
+procedure TRechner.btnZahl1Click(Sender: TObject);
 begin
   iZahl1 := 1;
   if bOperator = False then
     begin
       iEingabe1 := iZahl1;
-      Label1.Caption := IntToStr(iEingabe1);
+      lblEingabe1.Caption := IntToStr(iEingabe1);
     end
     else
     begin
       iEingabe2 := iZahl1;
-      Label3.Caption := IntToStr(iEingabe2);
+      lblEingabe2.Caption := IntToStr(iEingabe2);
     end;
 end;
 
-procedure TForm1.Button2Click(Sender: TObject);
+procedure TRechner.btnZahl2Click(Sender: TObject);
 begin
   iZahl2 := 2;
   if bOperator = False then
     begin
       iEingabe1 := iZahl2;
-      Label1.Caption := IntToStr(iEingabe1);
+      lblEingabe1.Caption := IntToStr(iEingabe1);
     end
     else
     begin
       iEingabe2 := iZahl2;
-      Label3.Caption := IntToStr(iEingabe2);
+      lblEingabe2.Caption := IntToStr(iEingabe2);
     end;
-
 end;
 
-procedure TForm1.Button3Click(Sender: TObject);
+procedure TRechner.btnZahl3Click(Sender: TObject);
 begin
   iZahl3 := 3;
+  if bOperator = False then
+    begin
+      iEingabe1 := iZahl3;
+      lblEingabe1.Caption := IntToStr(iEingabe1);
+    end
+  else
+    begin
+      iEingabe2 := iZahl3;
+      lblEingabe2.Caption := IntToStr(iEingabe2);
+    end;
 end;
 
-procedure TForm1.Button4Click(Sender: TObject);
+procedure TRechner.btnZahl4Click(Sender: TObject);
 begin
   iZahl4 := 4;
+  if bOperator = False then
+    begin
+      iEingabe1 := iZahl4;
+      lblEingabe1.Caption := IntToStr(iEingabe1);
+    end
+  else
+    begin
+      iEingabe2 := iZahl4;
+      lblEingabe2.Caption := IntToStr(iEingabe2);
+    end;
 end;
 
-procedure TForm1.Button5Click(Sender: TObject);
+procedure TRechner.btnZahl5Click(Sender: TObject);
 begin
   iZahl5 := 5;
+  if bOperator = False then
+    begin
+      iEingabe1 := iZahl5;
+      lblEingabe1.Caption := IntToStr(iEingabe1);
+    end
+  else
+    begin
+      iEingabe2 := iZahl5;
+      lblEingabe2.Caption := IntToStr(iEingabe2);
+    end;
 end;
 
-procedure TForm1.Button6Click(Sender: TObject);
+procedure TRechner.btnZahl6Click(Sender: TObject);
 begin
   iZahl6 := 6;
+  if bOperator = False then
+    begin
+      iEingabe1 := iZahl6;
+      lblEingabe1.Caption := IntToStr(iEingabe1);
+    end
+  else
+    begin
+      iEingabe2 := iZahl6;
+      lblEingabe2.Caption := IntToStr(iEingabe2);
+    end;
 end;
 
-procedure TForm1.Button7Click(Sender: TObject);
+procedure TRechner.btnZahl7Click(Sender: TObject);
 begin
   iZahl7 := 7;
+  if bOperator = False then
+    begin
+      iEingabe1 := iZahl7;
+      lblEingabe1.Caption := IntToStr(iEingabe1);
+    end
+  else
+    begin
+      iEingabe2 := iZahl7;
+      lblEingabe2.Caption := IntToStr(iEingabe2);
+    end;
 end;
 
-procedure TForm1.Button8Click(Sender: TObject);
+procedure TRechner.btnZahl8Click(Sender: TObject);
 begin
   iZahl8 := 8;
+  if bOperator = False then
+    begin
+      iEingabe1 := iZahl8;
+      lblEingabe1.Caption := IntToStr(iEingabe1);
+    end
+  else
+    begin
+      iEingabe2 := iZahl8;
+      lblEingabe2.Caption := IntToStr(iEingabe2);
+    end;
 end;
 
-procedure TForm1.Button9Click(Sender: TObject);
+procedure TRechner.btnZahl9Click(Sender: TObject);
 begin
   iZahl9 := 9;
+  if bOperator = False then
+    begin
+      iEingabe1 := iZahl9;
+      lblEingabe1.Caption := IntToStr(iEingabe1);
+    end
+  else
+    begin
+      iEingabe2 := iZahl9;
+      lblEingabe2.Caption := IntToStr(iEingabe2);
+    end;
 end;
 
-procedure TForm1.Button10Click(Sender: TObject);
+procedure TRechner.btnZahl0Click(Sender: TObject);
 begin
-  iZahl10 := 0;
+  iZahl0 := 0;
+  if bOperator = False then
+    begin
+      iEingabe1 := iZahl0;
+      lblEingabe1.Caption := IntToStr(iEingabe1);
+    end
+  else
+    begin
+      iEingabe2 := iZahl0;
+      lblEingabe2.Caption := IntToStr(iEingabe2);
+    end;
 end;
 
 end.
